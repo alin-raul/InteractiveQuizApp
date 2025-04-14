75177a7d185eb66756bd950b38699fc16b0cf004
Raul <alinnsts@gmail.com>
2025-04-14 20:57:14 +0300

    URL Log

--- src/app/fetchData/page.tsx
+++ src/app/fetchData/page.tsx
@@ -5,6 +5,15 @@ let cachedQuizData: QuizData | null = null;
 export const fetchQuizData = async () => {
   const baseUrl = process.env.NEXT_PUBLIC_APP_URL;
 
+  console.log(">>> Using NEXT_PUBLIC_APP_URL:", baseUrl);
+
+  if (!baseUrl) {
+    console.error(
+      "FATAL ERROR: NEXT_PUBLIC_APP_URL is not defined in the environment!"
+    );
+    throw new Error("Application base URL is not configured.");
+  }
+
   if (cachedQuizData) {
     return cachedQuizData;
   }
